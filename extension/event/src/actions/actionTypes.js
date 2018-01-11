9c0ec35f8153f7047f9430d3ef188890399eaa1c
Andrea Ceccarelli <gltch2003@gmail.com>
2018-01-11 12:24:10 +0100

    Add api and api reducer alpha

--- extension/event/src/actions/actionTypes.js
+++ extension/event/src/actions/actionTypes.js
@@ -1,1 +1,3 @@
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_ONLOAD = 'AUTH_ONLOAD';
+export const AUTH_FAIL = 'AUTH_FAIL';
--- extension/event/src/reducers/login.js
+++ extension/event/src/reducers/login.js
@@ -1,10 +1,16 @@
-import { AUTH_SUCCESS } from '../actions/actionTypes';
+import { AUTH_SUCCESS, AUTH_ONLOAD, AUTH_FAIL } from '../actions/actionTypes';
 
 const initialState = '';
 
 export default (state = initialState, action) => {
     switch (action.type) {
-    case AUTH_SUCCESS:
-      
+    case AUTH_SUCCESS:  
+        return state;
+    case AUTH_ONLOAD:  
+        return state;
+    case AUTH_FAIL:  
+        return state;
+    default:
+        return state;
     }
 }
